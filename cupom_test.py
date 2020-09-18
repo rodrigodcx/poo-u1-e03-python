747ed07a8bd3b5dded7f377d04ba7d5cc7b2da04
Rodrigo Vilar <67280390+rodrigodcx@users.noreply.github.com>
2020-09-18 09:54:54 -0300

    Teste para o Exercício 3

--- cupom_test.py
+++ cupom_test.py
@@ -13,7 +13,7 @@ observacao = "Loja 1317 (PDP)"
 cnpj = "42.591.651/0797-34"
 inscricao_estadual = "244.898.500.113"
 
-def test_exercicio1():
+def test_loja_completa():
     assert cupom.imprime_dados_loja() == '''Arcos Dourados Com. de Alimentos LTDA
 Av. Projetada Leste, 500 EUC F32/33/34
 Br. Sta Genebra - Campinas - SP
@@ -23,41 +23,54 @@ CNPJ: 42.591.651/0797-34
 IE: 244.898.500.113
 '''
 
-def test_exercicio2_tudo_vazio():
+def test_nome_vazio:
     global nome_loja
-    global logradouro
-    global numero
-    global complemento
-    global bairro
-    global municipio
-    global estado
-    global cep
-    global telefone
-    global observacao
-    global cnpj
-    global inscricao_estadual
-    
     nome_loja = ""
+    assert cupom.imprime_dados_loja() == '''O campo nome da loja é obrigatório'''
+    nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
+
+def test_logradouro_vazio:
+    global logradouro
     logradouro = ""
+    assert cupom.imprime_dados_loja() == '''O campo logradouro do endereço é obrigatório'''
+    logradouro = "Av. Projetada Leste"
+
+def test_numero_zero:
+    global numero
     numero = 0
-    complemento = ""
-    bairro = ""
-    municipio = ""
-    estado = ""
-    cep = ""
-    telefone = ""
-    observacao = ""
-    cnpj = ""
-    inscricao_estadual = ""
-
-    assert cupom.imprime_dados_loja() == '''
-, 0 
- -  - 
-CEP: Tel 
-
-CNPJ: 
-IE: 
+    assert cupom.imprime_dados_loja() == '''Arcos Dourados Com. de Alimentos LTDA
+Av. Projetada Leste, s/n EUC F32/33/34
+Br. Sta Genebra - Campinas - SP
+CEP:13080-395 Tel (19) 3756-7408
+Loja 1317 (PDP)
+CNPJ: 42.591.651/0797-34
+IE: 244.898.500.113
 '''
+    numero = 500
+
+def test_municipio_vazio:
+    global municipio
+    municipio = ""
+    assert cupom.imprime_dados_loja() == '''O campo município do endereço é obrigatório'''
+    municipio = "Campinas"
+
+def test_estado_vazio:
+    global estado
+    estado = ""
+    assert cupom.imprime_dados_loja() == '''O campo estado do endereço é obrigatório'''
+    estado = "SP"
+
+def test_cnpj_vazio:
+    global cnpj
+    cnpj = ""
+    assert cupom.imprime_dados_loja() == '''O campo CNPJ da loja é obrigatório'''
+    cnpj = "42.591.651/0797-34"
+
+def test_cnpj_vazio:
+    global inscricao_estadual
+    inscricao_estadual = ""
+    assert cupom.imprime_dados_loja() == '''O campo inscrição estadual da loja é obrigatório'''
+    inscricao_estadual = "244.898.500.113"
 
 def test_exercicio2_customizado():
     global nome_loja
